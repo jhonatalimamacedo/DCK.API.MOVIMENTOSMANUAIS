54ea14efeee635cce8cef7e5f9152ade33d90997
Jhonata Lima Macedo <jhonatamacedo@brq.com>
2022-10-09 03:23:59 -0300

    infra

---
+++ Infrastructure/Context.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> opts) : base(opts) { }
+
+        public DbSet<MOVIMENTO_MANUAL> mOVIMENTO_MANUAL { get; set; }
+        public DbSet<PRODUTO> pRODUTO { get; set; }
+        public DbSet<PRODUTO_COSIF> pRODUTO_COSIF { get; set; }
+
+    }
+}
---
+++ Infrastructure/StatupSetup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class StatupSetup
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<Context>(options =>
+            options.UseSqlite(connectionString));
+    }
+}
